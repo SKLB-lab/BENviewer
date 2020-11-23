6d00432d8d092587acacbbcfe161e6b913d4f196
root <admin@example.com>
2020-11-23 14:03:04 +0800

    update

--- web/BEN.regnetworks/line/plot.php
+++
@@ -1,3 +0,0 @@
-<?php
-    include '../../plot2.php';
-?>
--- web/db.php
+++
@@ -1,9 +0,0 @@
-<?php
-    $url=explode('/',$_SERVER["REQUEST_URI"]);
-    $db=mysqli_connect('','','',$url[1]);
-	$db->query("set names utf8");
-	if(!$db)
-	{
-		die('ERROR'.mysqli_error());
-	}
-?>
